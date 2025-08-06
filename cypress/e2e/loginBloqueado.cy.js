2d35ec93d0288d964ead2a8d95848457bf037249
awpadilha <awpadilha.dev@gmail.com>
2025-08-06 20:21:47 -0300

    Testes Básicos Cypress

---
+++ cypress/e2e/loginBloqueado.cy.js
@@ -0,0 +1,15 @@
+describe('Login', () => {
+  it('Login bloqueado deve apresentar mensagem de erro', () => {
+    
+    //Arrange
+    cy.visit('http://localhost:8080')
+    
+    //Act
+    cy.get(':nth-child(1) > label').click().type('admin@teste.com')
+     cy.get(':nth-child(2) > label').click().type('senaInvalida')
+     cy.get('.btn').click()
+     
+     //Assert
+    cy.get('.toast').should('be.visible')
+  })
+})
