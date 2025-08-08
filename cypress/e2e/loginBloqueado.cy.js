eeda69053717014cb6344613e939ca8e4d0409e3
AndradeVan <vander_lei_andrade@outlook.com>
2025-08-08 10:46:34 -0300

    Adicionar teste para funcionalidade de bloqueio de login.

--- cypress/e2e/loginBloqueado.cy.js
+++ cypress/e2e/loginBloqueado.cy.js
@@ -1,15 +1,27 @@
-describe('Login', () => {
-  it('Login bloqueado deve apresentar mensagem de erro', () => {
-    
+describe('Bloqueio de Login', () => {
+
+  beforeEach(() => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/api/auth/unblock',
+      body: {
+        "email": 'teste@teste.com'
+      }
+    })
+  })
+  it('Deve apresentar uma mensagem de erro ao tentar fazer login três vezes com senha inválida', () => {
+
     //Arrange
     cy.visit('http://localhost:8080')
-    
-    //Act
-    cy.get(':nth-child(1) > label').click().type('admin@teste.com')
-     cy.get(':nth-child(2) > label').click().type('senaInvalida')
-     cy.get('.btn').click()
+
+    //Act 
+    cy.get('label[for="email"]').click().type('teste@teste.com')
+    cy.get('label[for="password"]').click().type('senaInvalida')
+    cy.get('.btn').click()
+    cy.get('.btn').click()
+    cy.get('.btn').click()
      
-     //Assert
-    cy.get('.toast').should('be.visible')
+    //Assert
+    cy.get('.toast').should('contains.text','Usuário bloqueado devido a múltiplas tentativas de login inválidas')
   })
 })
